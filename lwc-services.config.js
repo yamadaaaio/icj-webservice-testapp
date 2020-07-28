0a0469bd70a25edbce285d151fc06825f2de15a3
hsol-myamada <hsol-myamada@mx2.toyota-cs.com>
2020-07-28 17:58:39 +0900

    commit

--- lwc-services.config.js
+++ lwc-services.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // Find the full example of all available configuration options at
 // https://github.com/muenzpraeger/create-lwc-app/blob/main/packages/lwc-services/example/lwc-services.config.js
 module.exports = {
