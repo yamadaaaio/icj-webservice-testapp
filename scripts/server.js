a37ce60e2644b518b648ab8657b36e2b02b0c1f7
hsol-myamada <hsol-myamada@mx2.toyota-cs.com>
2020-07-28 22:59:40 +0900

    commit

--- scripts/server.js
+++ scripts/server.js
@@ -3,8 +3,10 @@ const compression = require('compression');
 const helmet = require('helmet');
 const express = require('express');
 const path = require('path');
+const jsforceAjaxProxy = require('jsforce-ajax-proxy');
 
 const app = express();
+app.all('/proxy/?*', jsforceAjaxProxy());
 app.use(helmet());
 app.use(compression());
 
--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -1,14 +1,19 @@
-import { jsforce } from 'jsforce';
+import jsforce from 'jsforce';
 import { LightningElement } from 'lwc';
 
 export default class App extends LightningElement {
-    handleClick() {
-        jsforce.browser.init({
+    handleClick(event) {
+        const jsforceOptions = {
             clientId:
                 '3MVG9n_HvETGhr3DXo.3MwroNCdEYZwkgH93FbdMarRV_9dFU5IBqP1VU9CvXvMThsjkuLPCD23M3lubW1oEd',
-            redirectUri: 'https://evening-chamber-04072.herokuapp.com/'
-        });
+            redirectUri: 'https://evening-chamber-04072.herokuapp.com/',
+            proxyUrl: 'http://localhost:3001/proxy/',
+            loginUrl:
+                event.target.dataset.env === 'sandbox'
+                    ? 'https://test.salesforce.com'
+                    : 'https://login.salesforce.com'
+        };
 
-        jsforce.browser.login('http://login.salesforce.com');
+        jsforce.browser.login(jsforceOptions);
     }
 }
