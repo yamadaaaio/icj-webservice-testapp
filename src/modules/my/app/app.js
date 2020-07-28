b2d0e7ff6e6cfc8674f9b918adad620b7ec7fa2f
hsol-myamada <hsol-myamada@mx2.toyota-cs.com>
2020-07-28 17:14:35 +0900

    commit

--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -1,3 +1,14 @@
+import { jsforce } from 'jsforce';
 import { LightningElement } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+    handleClick() {
+        jsforce.browser.init({
+            clientId:
+                '3MVG9n_HvETGhr3DXo.3MwroNCdEYZwkgH93FbdMarRV_9dFU5IBqP1VU9CvXvMThsjkuLPCD23M3lubW1oEd',
+            redirectUri: 'https://evening-chamber-04072.herokuapp.com/'
+        });
+
+        jsforce.browser.login('http://login.salesforce.com');
+    }
+}
